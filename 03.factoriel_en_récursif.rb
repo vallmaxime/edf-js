cbd289bae8185c9bc15bb38ce65b31444df6845b
Maxime <valliemaxime@gmail.com>
2020-05-18 16:09:30 +0200

    add recursiv facto

---
+++ 03.factoriel_en_récursif.rb
@@ -0,0 +1,16 @@
+let [nd, sc, num] = process.argv;
+
+
+
+var facto = function(x) {
+    if (x < 1) {
+      console.log("Il faut donner un chiffre au moins de 1 pour avoir l'effet de ce script");
+    } else if (x == 1) {
+        return 1;
+    } else {
+        return (x * facto(x - 1));
+    }
+}
+
+res = facto(num);
+console.log(res);
